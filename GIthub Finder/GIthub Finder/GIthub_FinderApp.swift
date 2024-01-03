ffe8d1584f16072960ea07039c4ec2b0f9294064
Rahul Ramakrishnan <rahulrhs237@outlook.com>
2024-01-03 21:50:46 +0530

    Initial

---
+++ GIthub Finder/GIthub Finder/GIthub_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIthub_FinderApp.swift
+//  GIthub Finder
+//
+//  Created by UserTest on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GIthub_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
